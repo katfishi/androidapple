9d16425a7afcc25c62fa3aae564020c4c89349a0
Katheryn Shi <kshi@coursera.org>
2015-11-02 22:38:38 -0800

    Adding Pods

---
+++ Pods/Target Support Files/Pods-AndroidApples/Pods-AndroidApples-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// React
+#define COCOAPODS_POD_AVAILABLE_React
+#define COCOAPODS_VERSION_MAJOR_React 0
+#define COCOAPODS_VERSION_MINOR_React 11
+#define COCOAPODS_VERSION_PATCH_React 0
+
+// React/Core
+#define COCOAPODS_POD_AVAILABLE_React_Core
+#define COCOAPODS_VERSION_MAJOR_React_Core 0
+#define COCOAPODS_VERSION_MINOR_React_Core 11
+#define COCOAPODS_VERSION_PATCH_React_Core 0
+
+// React/RCTWebSocket
+#define COCOAPODS_POD_AVAILABLE_React_RCTWebSocket
+#define COCOAPODS_VERSION_MAJOR_React_RCTWebSocket 0
+#define COCOAPODS_VERSION_MINOR_React_RCTWebSocket 11
+#define COCOAPODS_VERSION_PATCH_React_RCTWebSocket 0
+
